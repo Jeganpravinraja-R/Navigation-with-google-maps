5534650298ddad8b4cf276bca978e5cbab6ac9a7
Jeganpravinraja-R <67156409+Jeganpravinraja-R@users.noreply.github.com>
2020-07-02 19:25:13 +0530

    Add files via upload

---
+++ config/colors.js
@@ -0,0 +1,10 @@
+export default {
+  primary: "#4ecdc4",
+  secondary: "#fc5c65",
+  black: "#000",
+  white: "#fff",
+  dark: "#0c0c0c",
+  light: "#f8f4f4",
+  medium: "#6e6969",
+  danger: "#ff5252",
+};
