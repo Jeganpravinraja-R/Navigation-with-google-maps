0f08d5a762f132ce44050e89345a3055e88d92d5
Jeganpravinraja-R <67156409+Jeganpravinraja-R@users.noreply.github.com>
2020-07-02 19:31:17 +0530

    Add files via upload

---
+++ screens/ProfileScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+function ProfileScreen(props) {
+  return (
+    <View style={styles.container}>
+        <Text> profile ProfileScreen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+  }
+});
+
+export default ProfileScreen;
